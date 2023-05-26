d51e76b065a708a9c879fc3ce002d78b032b0a20
srsempere <samueljose.garcia@iesdonana.org>
2023-05-26 21:00:11 +0200

    Creado el juego de ahorcado

---
+++ script.js
@@ -0,0 +1,44 @@
+new Vue({
+    el: "#app",
+    data: () => ({
+        words: [],
+        newWord: "",
+    }),
+    mounted() {
+        this.refreshWords();
+    },
+    methods: {
+        refreshWords() {
+            this.words = getWords();
+        },
+        saveWord() {
+            // Clean it again ._.
+            this.deleteWhiteSpaces();
+            const word = this.newWord.toUpperCase();
+            // Only save if it does not exist
+            if (this.words.indexOf(word) === -1) {
+                this.words.push(word);
+                saveWords(this.words);
+                this.newWord = "";
+            } else {
+                Swal.fire("The word already exists");
+            }
+        },
+        async deleteWord(index) {
+            const result = await Swal.fire({
+                title: 'Deleting word',
+                text: "Are you sure?",
+                icon: 'question',
+                showCancelButton: true,
+                cancelButtonText: 'No, take me back',
+                confirmButtonText: 'Yes, delete it'
+            });
+            if (!result.value) return;
+            this.words.splice(index, 1);
+            saveWords(this.words);
+        },
+        deleteWhiteSpaces() {
+            this.newWord = this.newWord.replace(/ /g, "")
+        }
+    }
+});
